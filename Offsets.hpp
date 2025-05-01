0d20f0e292505f390ff69103fb005ad6be5d4212
YuB-X <128683039+YuB-W@users.noreply.github.com>
2025-05-01 17:58:48 +0200

    Create Offsets.hpp

---
+++ Offsets.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "Windows.h"
+#include <cstdint>
+
+namespace ofssetss_new
+{
+    // whitelist stuff
+    static constexpr auto set_insert = 0xba15b0;
+    static constexpr auto whitelist_page = 0x2856b8;
+    static constexpr auto Bitmap = 0x299ac0;
+    constexpr uint64_t key = 0x7b822ce4;
+}
