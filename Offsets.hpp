5dcc98ad8fc340c918bfad0086eccf5ca795d246
YuB-X <128683039+YuB-W@users.noreply.github.com>
2025-05-08 08:54:56 +0200

    Update Offsets.hpp

--- Offsets.hpp
+++ Offsets.hpp
@@ -7,8 +7,7 @@
 namespace ofssetss_new
 {
     // whitelist stuff
-    static constexpr auto set_insert = 0xba15b0;
-    static constexpr auto whitelist_page = 0x2856b8;
-    static constexpr auto Bitmap = 0x299ac0;
-    constexpr uint64_t key = 0x7b822ce4;
+    static constexpr auto set_insert = 0xe5c390;  // set_insert function 0xba15b0
+    static constexpr auto whitelist_page = 0x29d1d0;  // whitelist function (same as set_insert now) 0x2856b8
+    static constexpr auto Bitmap = 0x283ac0;  // whitelist function (same as set_insert now) 0x299ac0
 }
