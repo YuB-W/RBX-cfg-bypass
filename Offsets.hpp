a11a205e825b71c691d92975c0ada92288ba9a19
YuB-X <128683039+YuB-W@users.noreply.github.com>
2025-05-27 23:32:54 +0300

    Update Offsets.hpp

--- Offsets.hpp
+++ Offsets.hpp
@@ -2,12 +2,12 @@
 #include "Windows.h"
 #include <cstdint>
 
-// Hyprion Version: version-e00a4ca39fb04359
+// Hyprion Version: version-9f371db70cc24a92
 
 namespace ofssetss_new
 {
     // whitelist stuff
-   static constexpr auto set_insert = 0xE15010;  
-   static constexpr auto whitelist_page = 0x2730A8;  
-   static constexpr auto Bitmap = 0x2750F8; 
+   static constexpr auto set_insert = 0xDD2FB0;  
+   static constexpr auto whitelist_page = 0x2a66b8;  
+   static constexpr auto Bitmap = 0x2a6990; 
 }
