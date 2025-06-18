d50e5b2824ec1d37bf4c3c796efddcea8d2ffefd
YuB-X <128683039+YuB-W@users.noreply.github.com>
2025-06-18 11:36:57 +0300

    Update Offsets.hpp

--- Offsets.hpp
+++ Offsets.hpp
@@ -1,11 +1,13 @@
 #pragma once
 #include <cstdint>
 
+// Roblox Version: version-82f8ee8d17124507
+
 namespace unordered_set {
     using insert = void* (__fastcall*)(void*, void*, void*);
 }
 
 
-static const uintptr_t set_insert = 0xD0D0F0;
-static const uintptr_t Bitmap = 0x2A6938;
-static const uintptr_t whitelist_page = 0x2A5A70;
+static const uintptr_t set_insert = 0xDA4E70;
+static const uintptr_t Bitmap = 0x298668;
+static const uintptr_t whitelist_page = 0x297EC8;
