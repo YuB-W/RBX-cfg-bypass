3d6c4092817f22e1e3a1da916f40c22df26fe3b8
YuB-X <128683039+YuB-W@users.noreply.github.com>
2025-07-10 12:54:33 +0300

    Update Offsets.hpp

--- Offsets.hpp
+++ Offsets.hpp
@@ -1,13 +1,18 @@
+// YuB-X Version: 2.0.8
+// Roblox Version: version-765338e04cf54fde
+// Dump Time:      2025-07-09 22:43:00
 #pragma once
 #include <cstdint>
-
-// Roblox Version: version-82f8ee8d17124507
 
 namespace unordered_set {
     using insert = void* (__fastcall*)(void*, void*, void*);
 }
 
+const uintptr_t dumpsetinsert      = (0xCBAE30);
+const uintptr_t BitMap             = (0x297908);
+const uintptr_t whitelist          = (0x29AE70);
 
-static const uintptr_t set_insert = 0xDA4E70;
-static const uintptr_t Bitmap = 0x298668;
-static const uintptr_t whitelist_page = 0x297EC8;
+const uintptr_t WHITELIST_MODE_TAG   = (0xd0698f90);
+const uintptr_t CFG_PAGE_HASH_KEY    = (0xC9E94648);
+const uintptr_t CFG_VALIDATION_XOR   = (0xD);
+
